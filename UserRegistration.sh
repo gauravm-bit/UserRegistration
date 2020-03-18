b35d73da75fcacd73bc84aeeed528f733536fe7a
gauravm-bit <gauravwrk01@gmail.com>
2020-03-18 00:12:24 -0700

    ADDED: validate last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,14 +3,27 @@
 echo "Welcome to User Registration"
 
 function FirstName()
-		{
-			read -p "Enter the first name : " firstName
-			pattern="^[A-Z][a-z]{3,}$"
-			if [[ $firstName =~ $pattern ]]
-			then
-				echo "okay"
-			else
-				echo "not okay"
-			fi
-		}
+	{
+		read -p "Enter the first name : " firstName
+		pattern="^[A-Z][a-z]{3,}$"
+		if [[ $firstName =~ $pattern ]]
+		then
+			echo "okay"
+		else
+			echo "not okay"
+		fi
+	}
 FirstName
+
+function LastName()
+	{
+		read -p "Enter the last name : " lastName
+		pattern="^[A-Z][a-z]{3,}$"
+		if [[ $lastName =~ $pattern ]]
+		then
+			echo "okay"
+		else
+			echo "not okay"
+		fi
+	}
+LastName
